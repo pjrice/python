770a1c50bfe7e472a00013b475f4fabb807cdfdd
pjrice <pjrice@uw.edu>
2017-01-05 22:41:32 -0800

    test commit for python line ending changes

---
+++ python/test.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  5 22:39:53 2017
+
+@author: CCDLN-PC1
+"""
+
+#this is a test file for git to ensure python push/pull is functioning properly
+#after converting to LF line endings for linux compatibility
