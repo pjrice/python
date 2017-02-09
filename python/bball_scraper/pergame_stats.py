724abbbaa0d829fb61bd5ea2c1ac9fea0a7dd87e
pjrice <pjrice@uw.edu>
2017-02-08 17:12:02 -0800

    saves stats to csv, grabs player name

--- python/bball_scraper/pergame_stats.py
+++ python/bball_scraper/pergame_stats.py
@@ -1,5 +1,6 @@
 #scrape basketball-reference.com player "per game" stats
 
+import csv
 import urllib.request
 from bs4 import BeautifulSoup
 
@@ -8,7 +9,7 @@ soup = BeautifulSoup(data, "lxml")
 
 
 #a list of the stats we're grabbing
-statlist = ['season','age','team_id','lg_id','pos','g','gs','mp_per_g',
+statList = ['season','age','team_id','lg_id','pos','g','gs','mp_per_g',
             'fg_per_g','fga_per_g','fg_pct','fg3_per_g','fg3a_per_g','fg3_pct',
             'fg2_per_g','fg2a_per_g','fg2_pct','efg_pct','ft_per_g',
             'fta_per_g','ft_pct','orb_per_g','drb_per_g','trb_per_g',
@@ -16,18 +17,31 @@ statlist = ['season','age','team_id','lg_id','pos','g','gs','mp_per_g',
             'pts_per_g']
 
 #check the raw html for data-stat attrs with our stat names
-stats_html = [soup.find_all(attrs={"data-stat": statlist[s]}) for s in range(statlist.__len__())]
+statsHTML = [soup.find_all(attrs={"data-stat": statList[s]}) for s in range(statList.__len__())]
 
 #try to have stats returned as a nested list - a list for each stat.
 stats = list()
-for s in range(stats_html.__len__()):
+for s in range(statsHTML.__len__()):
     
-    stats.append([ss.getText().strip() for ss in stats_html[s]])
+    stats.append([ss.getText().strip() for ss in statsHTML[s]])
     
 #got eem
+
+#get player name and add a new column
+playerHTML = soup.find_all('h1',{'itemprop': 'name'})
+playerName = playerHTML[0].getText().strip()
+#got player name, now replicate to match number of rows and insert
     
-    
+#print stats to csv
+zipStats = zip(*stats) #needs the * to work properly
+with open('/media/storage/python/python/bball_scraper/stats.csv','w') as statsFile:
+    wr = csv.writer(statsFile)
+    for row in zipStats:
+        wr.writerow(row)
+
+
 #todo:
-    #get player name!
-    #format stats to be exported to csv
+    #get player name and add to stats
     #do the same for the per-game logs! example: http://www.basketball-reference.com/players/w/westbru01/gamelog/2009
+    
+    
